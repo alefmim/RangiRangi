c9e2c1d5417f140030f1d283aee4c4d0aba9cd77
alefmim <48165275+alefmim@users.noreply.github.com>
2019-10-31 04:29:15 +0330

    Update blog.py

--- debug/blog.py
+++ debug/blog.py
@@ -38,8 +38,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' # Database connectio
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Because we don't need it
 db = SQLAlchemy(app)
 
-jdatetime.set_locale('fa_IR') # This line can get removed without affecting the program or producing any errors!
-
 # Persian months
 months = {1:'فروردین', 2:'اردیبهشت', 3:'خرداد', 4:'تیر', 5:'مرداد', 6:'شهریور', 7:'مهر', 8:'آبان', 9:'آذر', 10:'دی', 11:'بهمن', 12:'اسفند'}
 
