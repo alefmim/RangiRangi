40c5db33560d469814558a758f5c52eba4ffa84d
alefmim <alefmim@hotmail.com>
2019-11-11 19:42:49 +0330

    Update blog.py

--- blog.py
+++ blog.py
@@ -180,7 +180,7 @@ def formatDateTime(strDateTime: str, strFormat: str) -> str:
 	months = {1:'فروردین', 2:'اردیبهشت', 3:'خرداد', 4:'تیر', 5:'مرداد', 6:'شهریور', 7:'مهر', 8:'آبان', 9:'آذر', 10:'دی', 11:'بهمن', 12:'اسفند'}
 	# Persian days
 	days = {5:'شنبه', 6:'یکشنبه', 0:'دوشنبه', 1:'سه شنبه', 2:'چهارشنبه', 3:'پنج شنبه', 4:'جمعه'}
-
+	
 	# Convert strDateTime to a date/time object
 	gdt = datetime.datetime.strptime(strDateTime, '%Y-%m-%d %H:%M:%S')
 	jdt = jdatetime.GregorianToJalali(gdt.year, gdt.month, gdt.day)
